030be1091d4093c0f1acfa9713b819792a264729
jokahara <jaakko.kahara@helsinki.fi>
2023-12-04 20:02:09 +0200

    Added Chapter 5

--- data/create_human.R
+++ data/create_human.R
@@ -1,4 +1,4 @@
-# Jaakko Kähärä, 27.11.2023, Assignment 4
+# Jaakko Kähärä, 4.12.2023, Assignment 4 and 5
 
 library(dplyr)
 library(readr)
@@ -15,7 +15,7 @@ summary(gii)
 
 # rename column names to shorter ones
 colnames(hd) <- c("HDI.Rank", "Country", "HDI", 'Life.Exp', 'Edu.Exp', 'Edu.Mean', 'GNI', 'GNI.Rank')
-colnames(gii) <- c("GII.Rank", "Country", "GII", 'Mat.Mor', 'Ado.Birth', 'Rep.Par', 'Edu2.F', 'Edu2.M', 'Labo.F', 'Labo.M')
+colnames(gii) <- c("GII.Rank", "Country", "GII", 'Mat.Mor', 'Ado.Birth', 'Parli.F', 'Edu2.F', 'Edu2.M', 'Labo.F', 'Labo.M')
 
 # add ratios of between female and male populations
 gii <- mutate(gii, "Edu2.FM" = Edu2.F / Edu2.M)
@@ -25,5 +25,20 @@ gii <- mutate(gii, "Labo.FM" = Labo.F / Labo.M)
 human = inner_join(hd, gii)
 dim(human) # 195 x 19
 
-# save to human.csv
+# variables to keep
+keep <- c("Country", "Edu2.FM", "Labo.FM", "Edu.Exp", "Life.Exp", "GNI", "Mat.Mor", "Ado.Birth", "Parli.F")
+human <- human[keep]
+
+# remove rows with missing values
+human <- filter(human, complete.cases(human))
+
+# remove regional observations (last 7 rows)
+last <- nrow(human) - 7
+human <- human[1:last, ]
+
+dim(human) # 155 x 9
+
+# save to data/human.csv
+setwd('data')
 write_csv(human, 'human.csv')
+setwd('..')
